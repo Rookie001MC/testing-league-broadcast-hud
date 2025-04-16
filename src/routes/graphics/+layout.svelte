3b7f45f1d9d89f8afae40f2e02222641dae727e5
Rookie Nguyễn <nguyenquocthang2004@gmail.com>
2025-04-16 22:13:27 +0700

    idk really

---
+++ src/routes/graphics/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    let { children} = $props()
+</script>
+
+<main id="graphics-container">
+{@render children()}
+</main>
+
+<style>
+    #graphics-container{
+        height: 1920px;
+        width: 1080px;
+        position: relative;
+        overflow: hidden;
+    }
+</style>
